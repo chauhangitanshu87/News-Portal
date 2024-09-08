857b9dd9e909b2dc005659528b529b16335f7dec
girishrana <ranagirish246@gmail.com>
2024-09-08 18:45:12 +0530

    2nd day commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,36 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
   render() {
     return (
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a className="navbar-brand" href="/">NewsApp</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
+          <a className="navbar-brand" href="/">NewsApp</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item active">
-        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="/about">About</a>
-      </li>
-     
-     
-    </ul>
-   
-  </div>
-</nav>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto mb-2 mb-lg-0">
+             
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+              
+
+              <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
+
+            </ul>
+
+          </div>
+        </nav>
       </div>
     )
   }
